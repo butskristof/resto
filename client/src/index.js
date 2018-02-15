590ee56352b84bad57aa9f08da917d07bcbedde7
Kristof Buts <kr.buts@gmail.com>
2018-02-15 19:57:37 +0100

    Implemented React router

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Resto from './Resto';
 import Edit from './edit/Edit';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<Resto />, document.getElementById('root'));
-ReactDOM.render(<Edit />, document.getElementById('root'));
+// ReactDOM.render(<Edit />, document.getElementById('root'));
+ReactDOM.render( (
+	<Router>
+		<Switch>
+			<Route exact path="/" component={Resto} />
+			<Route path="/edit" component={Edit} />
+			<Route path={"*"} component={Resto} />
+		</Switch>
+	</Router>
+), document.getElementById('root') );
+
 registerServiceWorker();
